077bdd01407d5ddfa25be2fc63890201606106b5
Shikha-28 <vshikha963@gmail.com>
2021-06-05 17:40:13 +0530

    added search option

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,59 +1,88 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../MainPageLayout';
-import {apiGet } from '../misc/config';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
-  const [input,setInput] = useState('');
-  const [results,setResults] = useState(null);
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
-  const onSearch =() => {
-   apiGet(`/search/show?q=${input}`).then(result => {
-     setResults(result);
-   });
+  const isShowsSearch = searchOption === 'shows';
+
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
 
   };
 
- const onInputChange = ev =>{
-   setInput(ev.target.value);
- };
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
 
-  const onKeyDown = ev =>{
-    if(ev.keyCode === 13){
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
       onSearch()
     }
   };
 
+  const onRadioChange = ev =>{
+    setSearchOption(ev.target.value);
+
+  }
+
+   console.log(searchOption);
+
   const renderResults = () => {
-    if(results && results.length === 0){
+    if (results && results.length === 0) {
       return <div>No results</div>;
     }
-    if (results && results.length > 0){
-      return (
-        <div>
-          {results.map(item => (
-            <div key = {item.show.id}>
-              {item.show.name}
-            </div>
-          ))}
-        </div>
-      );
-    }
+
+    if (results && results.length > 0) {
+      return results[0].show 
+       ? results.map(item => (<div key={item.show.id}>{item.show.name}</div>))
+       : results.map(item => (
+         <div key={item.person.id}>{item.person.name}</div>
+       
+       ));
+       }
+    
     return null;
   };
-  
+
 
   return (
-  <MainPageLayout>
-  <input type ="text" onChange={onInputChange} onKeyDown ={onKeyDown} value={input} />
-  <button type ="button" onClick ={onSearch}>
-    Search
+    <MainPageLayout>
+      <input type="text"
+        placeholder="Seach for something"
+        onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+
+      <div>
+        <label htmlFor="shows-search">
+          shows
+    <input id="shows-search" type="radio"  value="shows"
+    checked ={isShowsSearch}
+     onChange={onRadioChange}/>
+        </label>
+
+        <label htmlFor="actors-search">
+          actors
+    <input id="actors-search" type="radio" value="people"
+    checked ={!isShowsSearch}
+     onChange={onRadioChange} />
+        </label>
+      </div>
+
+
+      <button type="button" onClick={onSearch}>
+        Search
   </button>
-  {renderResults()}
+      {renderResults()}
 
 
-  </MainPageLayout>
+    </MainPageLayout>
   );
-  
+
 };
 
 export default Home
